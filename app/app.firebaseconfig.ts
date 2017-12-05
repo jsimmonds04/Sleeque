14f8b57ea28b5a292a52d9bf4d558243ef897580
Jonathan S <jsimmonds04@outlook.com>
2017-12-05 17:36:06 +0000

    Add files via upload

---
+++ app/app.firebaseconfig.ts
@@ -0,0 +1,8 @@
+export var config = {
+    apiKey: "AIzaSyD9aynQpaxbLAmL9yB99JRJ9o4TVWzTnbY",
+    authDomain: "sleeque-3418d.firebaseapp.com",
+    databaseURL: "https://sleeque-3418d.firebaseio.com",
+    projectId: "sleeque-3418d",
+    storageBucket: "sleeque-3418d.appspot.com",
+    messagingSenderId: "294552496916"
+  };
